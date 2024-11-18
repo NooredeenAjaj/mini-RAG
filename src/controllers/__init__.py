394e190fc321a540f8fe2567ee22873ffdb106c8
Noor E Mustafa <noor_eidirhn.mustafa6827@student.lu.se>
2024-11-18 14:33:46 +0100

    adding document processing

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,3 +1,4 @@
 # import dörren, den som kommer hit vet vilka classer exporterar jag från dir controllers
 from .DataController import DataController
 from .ProjectController import ProjectController
+from .ProcessController import ProcessController
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,2 @@
 from .enums.ResopnsEnums import ResponseSignal
+from .enums.ProcessingEnum import ProcessingEnum
