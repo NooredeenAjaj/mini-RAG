8e2ec99089cd07d3e531f8231521719ce66bd67a
Noor E Mustafa <noor_eidirhn.mustafa6827@student.lu.se>
2024-11-19 23:07:13 +0100

    connecting to monogdb

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
 from routes import base, data
 from dotenv import load_dotenv
-import os
+
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import get_settings
+
 
 load_dotenv(".env")
+
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_db_client():
+    settings = get_settings()
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongo_conn.close()
+
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
